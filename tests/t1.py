909229b2108b5c49075dd4bfa86496d37b767a0b
Roland Kwitt <rkwitt@gmx.at>
2024-11-22 19:56:54 +0100

    REM: utils.py

--- tests/t1.py
+++ tests/t1.py
@@ -2,13 +2,51 @@ from otter.test_files import test_case
 
 OK_FORMAT = False
 
-from torch.utils.data import DataLoader
-from utils import get_data
+import torch
+import torch.nn as nn
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.compose import make_column_transformer, make_column_selector
+from sklearn.model_selection import train_test_split
+
+from torch.utils.data import DataLoader,TensorDataset
 
 name = "Exercise 5.1"
 points = 4
 
-@test_case(points=3)
+
+def get_data():
+    # Load data
+    fuel = pd.read_json('assets/fuel.json')
+    X = fuel.copy()
+    y = X.pop('FE')
+
+    # Preprocess data
+    preprocessor = make_column_transformer(
+        (StandardScaler(),
+        make_column_selector(dtype_include=np.number)),
+        (OneHotEncoder(sparse_output=False),
+        make_column_selector(dtype_include=object)),
+    )
+    X = preprocessor.fit_transform(X)
+    y = np.log(y)
+    
+    # Split data into training/testing
+    X_trn, X_tst, y_trn, y_tst = train_test_split(X, y, test_size=0.33, random_state=42)
+    
+    # Create PyTorch datasets
+    ds_trn = TensorDataset(
+        torch.tensor(np.array(X_trn), dtype=torch.float32),
+        torch.tensor(np.array(y_trn), dtype=torch.float32))
+    ds_tst = TensorDataset(
+        torch.tensor(np.array(X_tst), dtype=torch.float32),
+        torch.tensor(np.array(y_tst), dtype=torch.float32))
+    return ds_trn, ds_tst
+
+
+@test_case(points=4)
 def test_1(train, env):
     _, ds_tst = get_data()
     dl_tst = DataLoader(ds_tst, batch_size=32, shuffle=False)
